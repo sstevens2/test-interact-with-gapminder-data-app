55ae8ee9696875f8abb1d48cccc13f04446a1a5e
Sarah Stevens <sarah.stevens@wisc.edu>
2022-05-31 11:00:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,43 +2,60 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(layout="wide")
+# set up the app with wide view preset and a title
+st.set_page_config(layout = "wide")
 st.title("Interact with Gapminder Data")
 
+# read in the tidy gapminder data
 df = pd.read_csv("Data/gapminder_tidy.csv")
 
+# get a list of all possible continents and metrics, for the widgets
 continent_list = list(df['continent'].unique())
 metric_list = list(df['metric'].unique())
+
+# map the actual data values to more readable strings
 metric_labels = {"gdpPercap": "GDP Per Capita", "lifeExp": "Average Life Expectancy", "pop": "Population"}
 
+# function to be used in widget argument format_func that maps metric vals to readable labes, using abv dict
 def format_metric(metric_raw):
     return metric_labels[metric_raw]
 
+# put all widgets in a sidebar with subtitle
 with st.sidebar:
     st.subheader("Configure the plot")
     continent = st.selectbox(label = "Choose a continent", options = continent_list)
-    metric = st.selectbox(label = "Choose a metric", options = metric_list, format_func=format_metric)
-    show_data = st.checkbox("Show Data?")
-    
+    metric = st.selectbox(label = "Choose a metric", options = metric_list, format_func = format_metric)
 
+# use selected values from widgets to filter dataset down to only the rows we need
 query = f"continent=='{continent}' & metric=='{metric}'"
 df_filtered = df.query(query)
 
+# select which countries should be displayed
 countries_list = list(df_filtered['country'].unique())
-countries = st.sidebar.multiselect(label = "Which countries should be plotted?", options = countries_list, default = countries_list)
+with st.sidebar:
+    countries = st.multiselect(label = "Which countries should be plotted?", options = countries_list, default = countries_list)
 df_filtered = df_filtered[df_filtered.country.isin(countries)]
 
-
+# select which years should be displayed
 year_min = int(df_filtered['year'].min())
 year_max = int(df_filtered['year'].max())
-years = st.sidebar.slider("Which years should be plotted", min_value = year_min, max_value = year_max, value = (year_min, year_max))
-df_filtered = df_filtered[ (df_filtered['year'] >= years[0]) & (df_filtered['year'] <= years[1])]
 
+with st.sidebar:
+    years = st.slider(label = "What years should be plotted?", min_value = year_min, max_value = year_max,
+                     value = (year_min, year_max))
+df_filtered = df_filtered[(df_filtered.year >= years[0]) & (df_filtered.year <= years[1])]
+
+
+# create plot of data
 title = f"{metric_labels[metric]} for countries in {continent}"
-fig = px.line(df_filtered, x = "year", y = "value", color = "country", title = title, labels={"value": f"{metric_labels[metric]}"})
-st.plotly_chart(fig, use_container_width=True)
-st.markdown(f"This plot shows the {metric_labels[metric]} from {years[0]} to {years[1]} for the following countries in {continent}: {', '.join(countries)}.")
+fig = px.line(df_filtered, x = "year", y = "value", color = "country", 
+              title = title, labels={"value": f"{metric_labels[metric]}"})
+st.plotly_chart(fig)  # display the plot
+
+st.markdown(f"This plot shows the {metric_labels[metric]} for countries in {continent}")
+
+with st.sidebar:
+    show_data = st.checkbox(label = "Show the data used to generate this plot", value = False)
 
 if show_data:
-    st.subheader("Data for the above plot")
     st.dataframe(df_filtered)
